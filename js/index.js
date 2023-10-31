29a03367c1e21f877237f0a72acd3c9c9ca94ade
UTMIan <UTM20040084@utma.edu.mx>
2023-10-31 12:29:50 -0600

    Login registrando

--- js/index.js
+++ js/index.js
@@ -1,20 +1,74 @@
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyB4anY-9nz9mJXONJGtLFZmDB6TLusGuXM",
+    authDomain: "dbfishing-f4534.firebaseapp.com",
+    projectId: "dbfishing-f4534",
+    storageBucket: "dbfishing-f4534.appspot.com",
+    messagingSenderId: "958177801466",
+    appId: "1:958177801466:web:8c24e9c487241172ac4543"
+};
+
+// Inicializacion Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+// Obtiene una instancia de autenticación
+const auth = firebase.auth(app);
+
 document.addEventListener("DOMContentLoaded", function() {
     const nextButton = document.getElementById("nextButton");
-    const passwordForm = document.querySelector(".password-form");
     const emailInput = document.getElementById("email");
     const passwordInput = document.getElementById("password");
+    const backButton = document.getElementById("backButton");
+    const loginButton = document.getElementById("loginButton");
 
     // Agrega un manejador de eventos al botón "Siguiente"
     nextButton.addEventListener("click", function() {
-        // Oculta el botón "Siguiente" y el campo de correo
-        nextButton.style.display = "none";
+        // Oculta el campo de entrada de correo
         emailInput.style.display = "none";
 
-        // Muestra el campo de contraseña con animación
-        passwordForm.style.maxHeight = "1000px"; // Establece la altura máxima
-        passwordForm.style.animation = "slide-up 0.5s";
-
-        // Enfoca el campo de contraseña
+        // Muestra el campo de contraseña y establece el foco en él
+        passwordInput.style.display = "block";
         passwordInput.focus();
+
+        // Oculta el botón "Siguiente" y muestra el botón "Iniciar Sesión"
+        nextButton.style.display = "none";
+        loginButton.style.display = "block";
+
+        // Muestra el botón de regreso
+        backButton.style.display = "block";
+    });
+
+    // Agrega un manejador de eventos al botón de "Regresar"
+    backButton.addEventListener("click", function() {
+        // Oculta el campo de contraseña
+        passwordInput.style.display = "none";
+
+        // Muestra el campo de entrada de correo
+        emailInput.style.display = "block";
+        emailInput.focus();
+
+        // Oculta el botón "Regresar" y muestra el botón "Siguiente"
+        backButton.style.display = "none";
+        loginButton.style.display = "none";
+        nextButton.style.display = "block";
+    });
+
+    // Registro de usuarios
+    loginButton.addEventListener('click', function() {
+        const email = emailInput.value;
+        const password = passwordInput.value;
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Usuario registrado con éxito
+                const user = userCredential.user;
+                console.log('Usuario registrado:', user);
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error('Error al registrar usuario:', errorCode, errorMessage);
+            });
     });
 });
